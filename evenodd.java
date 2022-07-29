5a5643a7ec25c21dd70d23b9ffc9091fe2a21e78
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-29 18:13:52 +0100

    oddeven

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+package hwevenodd;
+
+//import static java.lang.System.*;
+
+public class evenodd {
+    public static void main(String[] args) {
+        int a = 111;
+        if (a % 2 ==0) {
+            // a / 2 ==0
+            System.out.println("num is even");
+
+        } else {
+
+
+            System.out.println("num is odd");
+        }
+    }
+}
+
+
+
